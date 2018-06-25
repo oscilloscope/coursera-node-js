413938591224c96c1699a91e8998760b54184a56
SALMAN_OGUZHAN <ogushan.salman@cs.com.tr>
2018-06-25 15:50:38 +0200

    mongodb example

---
+++ index.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dboper = require('./operations');
+const url = 'mongodb://localhost:27017/conFusion';
+
+MongoClient.connect(url, (err, db) => {
+
+    assert.equal(err, null);
+    console.log("connected correctly to server");
+
+    dboper.insertDocument(db, {name: "Vadonut", description: "Test"}, "dishes"
+, (result) => {
+    console.log("Insert Document: \n ", result.ops);
+    
+    dboper.findDocuments(db, "dishes", (docs) => {
+        console.log("Found Documents: \n", docs);
+
+        dboper.updateDocument(db, { name: "Vadonut"}, {description: "Updated Test"}, "dishes",
+    (result) => {
+        console.log("Updated Document: \n", result.result, " deneme deneme");
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Updated Documents: \n", docs);
+
+            db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
+                db.close();
+            });
+        });
+    });
+    });
+})
+});
